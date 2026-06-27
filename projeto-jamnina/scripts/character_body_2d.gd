12a01212f99723fc4cd1074e998ac4afa63cad2d
Luis Terasawa <tterasawa91@gmail.com>
2026-06-27 15:10:44 -0300

    Adicionados os spites de parado, andar e pular.

--- projeto-jamnina/scripts/character_body_2d.gd
+++ projeto-jamnina/scripts/character_body_2d.gd
@@ -1,19 +1,22 @@
 extends CharacterBody2D
 
-
 var SPEED = 500.0
 const JUMP_VELOCITY = -800.0
 var dash = false
 var pode
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor() and not dash:
 		velocity += get_gravity() * delta * 1.9
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("cima") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 	if is_on_floor():
 		pode = true
+		
 	var direcao
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -27,7 +30,18 @@ func _physics_process(delta: float) -> void:
 				direcao = "esquerda"
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-			
+	
+	if not is_on_floor() == true:
+		$AnimatedSprite2D.play('pular')
+	elif direcao == "direita":
+		$AnimatedSprite2D.flip_h = false 
+		$AnimatedSprite2D.play('andar')
+	elif direcao == "esquerda":
+		$AnimatedSprite2D.flip_h = true 
+		$AnimatedSprite2D.play('andar')
+	else:
+		$AnimatedSprite2D.play('parado')
+		
 	if Input.is_action_just_pressed("dash") and pode:
 			$Timer.start()
 			pode = false
@@ -38,6 +52,7 @@ func _physics_process(delta: float) -> void:
 				velocity.x = SPEED
 			elif direcao == "esquerda":
 				velocity.x = -SPEED
+	
 	move_and_slide()
 
 
