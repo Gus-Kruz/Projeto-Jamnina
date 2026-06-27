d77a5abe70b7d2ebf0e8fbf07536054b9c6f93b9
gus_kruz <gus@gusloq.com>
2026-06-27 18:12:55 -0300

    feat(censurador): progresso em censurador

--- projeto-jamnina/scripts/censurador.gd
+++ projeto-jamnina/scripts/censurador.gd
@@ -1,25 +1,54 @@
 extends CharacterBody2D
 
+@export var patrol_path : Path2D
+@onready var anim_state = $AnimatedSprite2D.get("andar1")
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var run_speed = 25.0
 
+enum states {PATROL, CHASE}
+var state = states.PATROL
+var target = null
+var player = null
+var current_patrol_point = 0
+var patrol_points = []
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _ready():
+	if patrol_path:
+		patrol_points = patrol_path.curve.get_baked_points()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	choose_action()
+	if target:
+		if target.x > position.x:
+			$AnimatedSprite2D.scale.x = 1
+			$DetectRadius/Visao.scale.x = 1
+		elif target.x < position.x:
+			$AnimatedSprite2D.scale.x = -1
+			$DetectRadius/Visao.scale.x = -1
+		velocity = position.direction_to(target) * run_speed
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if velocity.length() > 0:
+		anim_state.travel("andar1")
 	move_and_slide()
+
+func choose_action():
+	match state:
+		states.PATROL:
+			if !patrol_path:
+				anim_state.travel("andar1")
+				target = null
+				return
+			target = patrol_points[current_patrol_point]
+			if position.distance_to(target) < 5:
+				current_patrol_point = wrapi(current_patrol_point + 1, 0, patrol_points.size())
+		states.CHASE:
+			target = player.position
+
+func _on_detect_radius_body_entered(body):
+	player = body
+	state = states.CHASE
+
+func _on_detect_radius_body_exited(body):
+	player = null
+	state = states.PATROL
