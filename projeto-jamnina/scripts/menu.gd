0d577f5e9dce98d61a52f571e9b47f31b2255254
gus_kruz <gus@gusloq.com>
2026-06-27 17:14:10 -0300

    feat(censurador): :(

---
+++ projeto-jamnina/scripts/menu.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+func _on_configurar_pressed() -> void:
+	# get_tree().change_scene_to_file(configurações)
+	pass # Replace with function body.
+
+
+func _on_creditos_pressed() -> void:
+	# get_tree().change_scene_to_file(creditos)
+	pass # Replace with function body.
+
+
+func _on_jogar_pressed() -> void:
+	# get_tree().change_scene_to_file(jogar)
+	pass # Replace with function body.
+	
+func _on_sair_pressed() -> void:
+	get_tree().quit()
