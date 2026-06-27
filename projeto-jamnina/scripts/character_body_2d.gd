46cc2191c78194750859138d5d76cb9b74ee937b
Fernando Yuji Távora Pacheco <305817@dac.unicamp.br>
2026-06-27 17:24:19 -0300

    barraca adicionada

--- projeto-jamnina/scripts/character_body_2d.gd
+++ projeto-jamnina/scripts/character_body_2d.gd
@@ -1,22 +1,35 @@
 extends CharacterBody2D
 
 var SPEED = 500.0
-const JUMP_VELOCITY = -800.0
+const JUMP_VELOCITY = -8000.0
 var dash = false
 var pode
+var dentro
+var inside
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	if inside:
+		if Input.is_action_just_pressed("baixo"):
+			inside = false
+			$AnimatedSprite2D.z_index = 2
+			$AnimatedSprite2D.modulate = Color(1.0, 1.0, 1.0, 1.0)
+		return
 	if not is_on_floor() and not dash:
 		velocity += get_gravity() * delta * 1.9
 		
 	# Handle jump.
 	if Input.is_action_just_pressed("cima") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
+
 	if is_on_floor():
 		pode = true
-		
+	if Input.is_action_just_pressed("baixo") and dentro and is_on_floor():
+		velocity.x = 0
+		velocity.y = 0
+		pode = false
+		inside = true
+		$AnimatedSprite2D.modulate = Color(0.353, 0.353, 0.353, 1.0)
+		$AnimatedSprite2D.z_index = -1
 	var direcao
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -59,3 +72,11 @@ func _physics_process(delta: float) -> void:
 func _on_timer_timeout() -> void:
 	SPEED = 500
 	dash = false
+
+
+func _on_barraca_entrou() -> void:
+	dentro = true
+
+
+func _on_barraca_saiu() -> void:
+	dentro = false
