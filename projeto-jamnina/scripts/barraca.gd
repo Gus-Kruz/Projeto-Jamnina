580e5d6a75641301e362da31b1512d47735ad2a2
Gustavo Silva da Cruz <gustavocruz2006@gmail.com>
2026-06-28 00:48:20 -0300

    feat(menu): add styling/ feat(barraca): adds a variable for changing colors for each instance

--- projeto-jamnina/scripts/barraca.gd
+++ projeto-jamnina/scripts/barraca.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
+@export var cor : Color = Color.WHITE
 
+@onready var teto_tenda : Sprite2D = $Sprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	teto_tenda.modulate = cor
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,8 +18,6 @@ signal saiu
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "carteiro":
 		entrou.emit()
-
-
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "carteiro":
