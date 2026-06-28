4fa31ac4106dc92772d7053127a10d822a54a52a
Luis Terasawa <tterasawa91@gmail.com>
2026-06-28 17:08:50 -0300

    Pombo.

--- projeto-jamnina/scripts/pombo.gd
+++ projeto-jamnina/scripts/pombo.gd
@@ -1,14 +1,20 @@
 extends Node2D
-
+var carta = false
+@export var caminho :String
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimatedSprite2D.play('parado')
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("baixo") and carta:
+		pass
 	
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	$Label.visible = true
+	carta = true
+
+	
+		
+		
 	
