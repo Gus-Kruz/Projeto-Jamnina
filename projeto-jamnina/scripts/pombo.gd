fe18665e0f4547ef0c10946b2f2324add81d9c15
Luis Terasawa <tterasawa91@gmail.com>
2026-08-19 19:50:28 -0300

    umas linhas.

--- projeto-jamnina/scripts/pombo.gd
+++ projeto-jamnina/scripts/pombo.gd
@@ -1,14 +1,18 @@
-extends Node2D
+extends CharacterBody2D
+
 var carta = false
+var velocidade = 10
+var direcao = Vector2(-1,-1)
 @export var caminho :String
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	$AnimatedSprite2D.play('parado')
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("baixo") and carta:
 		$Label.visible = false
+		$AnimatedSprite2D.play('voando')
+		position = direcao * velocidade
 		get_tree().change_scene_to_file("res://cenas/carta/carta" + caminho + ".tscn")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
