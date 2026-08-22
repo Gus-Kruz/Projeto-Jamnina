0e27ee39c5e8b34e02d9b586e842ab27fe4785bb
Fernando Yuji <305817@dac.unicamp>
2026-08-21 23:02:22 -0300

    musica

---
+++ projeto-jamnina/scripts/nível_1.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_uwk_j_3c_zxmp_3_cut_net_finished() -> void:
+	$"UwkJ3cZx(mp3Cut_net)".play()
